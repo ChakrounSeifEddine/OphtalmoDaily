ddfc9af72739bd8d827eabb199c0e958d31f82f4
Chakroun Seif Eddine <Chakroun Seif Eddine>
2016-12-30 00:51:05 +0100

    Fin de la partie d'administration des consutlatation et la gestion des personnels

--- src/router/routing.js
+++ src/router/routing.js
@@ -16,12 +16,31 @@
     app.config(
 function($stateProvider, $urlRouterProvider){
 	
-	$stateProvider.state('city',{
+	$stateProvider.
+    state('city',{
 		url:"/city",
 		templateUrl: 'views/cityviews/city.html',
-            controller:'CityController'
+        controller:'CityController'
 
-	});
+	})
+    .state('consultation',{
+        url:"/consultation",
+        templateUrl:"views/consultation.html",
+        controller: 'consultationController'
+
+    })
+    .state('personnel',{
+        url:"/personnel",
+        templateUrl:"views/personnel.html",
+        controller:"personnelController"
+    })
+    .state("facturation",{
+        url:'/facturation',
+        templateUrl:"views/facturation.html",
+        controller:"facturationController"
+    })
+
+    ;
 	
 }
 
